2102bd0a15b92620411462b8e370ff12ddee2b2f
Test_User - test6 <test6@ecitele.com>
2019-03-14 19:07:54 +0200

    UTILITIES: added the ssh poc using paramiko and added a ping poc using pin3.

---
+++ sampleMain/utilities/sshParamiko.py
@@ -0,0 +1,19 @@
+import paramiko
+
+def sshTest2():
+    ip = '172.30.82.111'
+    port = 22
+    username = 'admin'
+    password = 'admin1'
+    #cmd = 'cd /config \n rm -rf device.conf* \n ls \n '
+    cmd = "lsh \n run show system status \n"
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip, port, username, password)
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    outlines = stdout.readlines()
+    resp = ''.join(outlines)
+
+    #print("after deleting all configuration files from ACTIVE MCP, the /config folder has:")
+    #print("-------------------------------------------------------------------------------")
+    print(resp)
