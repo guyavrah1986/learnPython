cdf98518156904f4d442fb34a1d29740a089852c
Test_User - test6 <test6@ecitele.com>
2019-03-14 14:35:38 +0200

    MAIN: added basic ssh session handling with paramiko.

--- sampleMain/main.py
+++ sampleMain/main.py
@@ -1,6 +1,29 @@
+import paramiko
+
+
+def sshTest2():
+    ip = '172.30.82.111'
+    port = 22
+    username = 'admin'
+    password = 'admin1'
+    #cmd = 'cd /config \n rm -rf device.conf* \n ls \n '
+    cmd = "lsh \n run show system status \n"
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip, port, username, password)
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    outlines = stdout.readlines()
+    resp = ''.join(outlines)
+
+    #print("after deleting all configuration files from ACTIVE MCP, the /config folder has:")
+    #print("-------------------------------------------------------------------------------")
+    print(resp)
+
+
 def main():
-    print("Hello World!")
+    print("main - start")
+    sshTest2()
 
-
+    print("main - end")
 if __name__ == "__main__":
     main()
