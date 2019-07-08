40a27e3f2195239e08a2049a42e55d7cecf2089e
Guy Avraham <guyavrah1986@gmail.com>
2019-07-08 22:01:36 +0300

    GIT_IGNORE and creating the setup.py file to run the main program.

--- learnPython/main.py
+++ learnPython/main.py
@@ -1,12 +1,15 @@
 import sys
 
-from learnPython.strings.stringsUsageExample import *
+from learnPython.strings.stringsUsageExample import strings_definition
 
 
-def main():
+def main(argv):
     func_name = "main - "
     print(func_name + "start")
+    print(func_name + "got command line arguments:\n")
+    print(argv)
+    print(func_name + "end")
 
 
 if __name__== "__main__":
-    main(sys.args, sys.argv)
+    main(sys.argv)
