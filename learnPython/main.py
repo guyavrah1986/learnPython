b68ce6a4ced805f84f97ce1ce419879ea0953818
Guy Avraham <guyavrah1986@gmail.com>
2019-07-08 21:49:57 +0300

    INIITIAL

---
+++ learnPython/main.py
@@ -0,0 +1,12 @@
+import sys
+
+from learnPython.strings.stringsUsageExample import *
+
+
+def main():
+    func_name = "main - "
+    print(func_name + "start")
+
+
+if __name__== "__main__":
+    main(sys.args, sys.argv)
---
+++ learnPython/strings/stringsUsageExample.py
@@ -0,0 +1,8 @@
+
+def strings_definition():
+    func_name = "strings_definition - "
+    print(func_name + "start")
+    str1 = "Doesn't"
+    print(func_name + "str1:" + str1)
+    str2 = 'Doesn\'t'
+    print(func_name + "str2:" + str2)
