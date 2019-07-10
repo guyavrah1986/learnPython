c87012b03b3550b28a925d3f431302b3d08da0f8
Guy Avraham <guyavrah1986@gmail.com>
2019-07-10 23:26:43 +0300

    STRING_USAGE:initial commit.

--- learnPython/strings/stringsUsageExample.py
+++ learnPython/strings/stringsUsageExample.py
@@ -2,6 +2,13 @@
 def strings_usage_example():
     func_name = "strings_definition - "
     print(func_name + "start")
+    string_definition_example()
+    print(func_name + "end")
+
+
+def string_definition_example():
+    func_name = "string_definition_example - "
+    print(func_name + "start")
     str1 = "Doesn't"
     print(func_name + "str1:" + str1)
     str2 = 'Doesn\'t'
