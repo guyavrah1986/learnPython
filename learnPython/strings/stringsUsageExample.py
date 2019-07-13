3ed08e706cb1fedc3d77930309c9c22e9585213e
Guy Avraham <guyavrah1986@gmail.com>
2019-07-13 14:06:20 +0300

    STRINGS:added a simple illustration for Python strings immutability.

--- learnPython/strings/stringsUsageExample.py
+++ learnPython/strings/stringsUsageExample.py
@@ -3,6 +3,7 @@ def strings_usage_example():
     func_name = "strings_definition - "
     print(func_name + "start")
     string_definition_example()
+    immutablity_of_string()
     print(func_name + "end")
 
 
@@ -13,3 +14,20 @@ def string_definition_example():
     print(func_name + "str1:" + str1)
     str2 = 'Doesn\'t'
     print(func_name + "str2:" + str2)
+
+
+def immutablity_of_string():
+    func_name = "immutablity_of_string - "
+    str1 = "old_str"
+    print(func_name + "str1 is:" + str1)
+    # if the below line will be commented out, the following error will rise:
+    # str1[1] = "b"
+    # TypeError: 'str' object does not support item assignment
+    # this is because strings are immuatable objects in Python
+    #str1[1] = "b"
+    #print(func_name + "now str1 is:" + str1)
+
+    # what actually happens here is that a "new string" is being constructed and
+    # then aassgined (thus overrides) the old value of str1. 
+    str1 = "new_str"
+    print(func_name + "now str1 is:" + str1)
