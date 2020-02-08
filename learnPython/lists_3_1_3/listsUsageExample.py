b9b25198cc19f36a51c148df654de6d7540484a5
Guy Avraham <guyavrah1986@gmail.com>
2020-02-08 14:52:06 +0200

    LIST:added an example of a list with different types of values.

--- learnPython/lists_3_1_3/listsUsageExample.py
+++ learnPython/lists_3_1_3/listsUsageExample.py
@@ -18,7 +18,10 @@ def basic_usage_lists():
     list2 = [5, 6, 7, 8]
     # list concatenation is also possible
     list3 = list1 + list2
-    print(func_name + "list3(list1+ + list2):" + str(list3))
+    print(func_name + "list3(list1 + list2):" + str(list3))
     # it is possible to define list of lists
     list4 = [list1, list3]
+    # list can hold values of different types
+    diff_types_list = [True, 12, "string", 17.15]
+    print(func_name + "the list with the different types of values is:\n" + str(diff_types_list))
     print(func_name + "list4 is:" + str(list4))
