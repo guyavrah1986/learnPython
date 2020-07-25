7b8d82ff4e36e4a111a69562ce1fd3dd1d4f7f81
Guy Avraham <guyavrah1986@gmail.com>
2020-07-25 10:00:07 +0300

    PASS_ARGUMENT_EXAMPLE:more example added.

--- learnPython/passingByAssginment/passingArgumentsExample.py
+++ learnPython/passingByAssginment/passingArgumentsExample.py
@@ -1,3 +1,32 @@
+###########################################################################################################
+# Pass by assignment:
+# In contrast to other programming languages, such as Java/C++, in Python there is NO really pass by
+# reference mechanism. Here it is called (and performed) as pass by assignment.
+# 1) If the object type that is being passed is IMMUTABLE:
+# 1.1) It can NOT be altered by the function, meaning any "reassignment" that takes place in the function
+# on the "function's argument" is relevant to this argument and NOT to the original variable (object) that
+# was sent to the function. This is due to the fact that a "copy" of the reference to the object is what
+# being sent to the function.
+# IMMUTABLE object in Python are:
+# - string (str)
+# - tuple
+# - primitive types: int, bool, decimal, float,
+# -range
+# 2) If the object type being passed is MUTABLE:
+# 2.1) It can be altered by the function, meaning, changing "part" of it (for example, an item in a list)
+# is possible, meaning, when passing a mutable object to a function, each change in the content of the
+# object itself, is reflected in the "original" object from the calling function.
+# 2.2) When passing a MUTABLE object to a function and it is being reassigned within it, the original
+# object from the calling function will NOT be altered.
+###########################################################################################################
+
+
+def change_string_content(string: str):
+    func_name = "change_string_content - "
+    print(func_name + "got string:" + string)
+    string += "_with_function_addition"
+    print(func_name + "after modifying the string in the function it is:" + string)
+
 
 def change_list_content(list_to_change: list):
     func_name = "change_list_content - "
@@ -7,8 +36,8 @@ def change_list_content(list_to_change: list):
 
 
 def change_list_assignment(list_to_change: list):
-    func_namme = "change_list_assignment - "
-    print(func_namme + "the list that was received had id:" + str(id(list_to_change)) + " and its content:"
+    func_name = "change_list_assignment - "
+    print(func_name + "the list that was received had id:" + str(id(list_to_change)) + " and its content:"
           + str(list_to_change))
     list_to_change = [1, 2, 3, 4, 5]
 
@@ -26,5 +55,17 @@ def pass_mutable_object_to_func():
     print(func_name + "end")
 
 
+def pass_immutable_object_to_func():
+    func_name = "pass_immutable_object_to_func - "
+    print(func_name + "start")
+    orig_str = "orig_str"
+    print(func_name + "BEFORE passing the original string to the function is it:" + orig_str)
+    # 1)
+    change_string_content(orig_str)
+    print(func_name + "AFTER passing the original string to the function is it:" + orig_str)
+    print(func_name + "end")
+
+
 if __name__ == "__main__":
     pass_mutable_object_to_func()
+    pass_immutable_object_to_func()
