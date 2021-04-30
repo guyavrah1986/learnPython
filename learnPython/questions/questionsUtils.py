3695144aed4bf90764b2ca2454f506ac46ec9b2f
Guy Avraham <Guy.Avraham@ecitele.com>
2021-04-30 13:08:28 +0300

    ROTATE_IMAGE:working code.

--- learnPython/questions/questionsUtils.py
+++ learnPython/questions/questionsUtils.py
@@ -7,3 +7,9 @@ def create_sub_string_chars_dict(sub_string: str) -> dict:
             ret_dict[c] = 1
 
     return ret_dict
+
+def print_two_dim_matrix(matrix: list):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print(matrix[i][j], end=' ')
+        print()
